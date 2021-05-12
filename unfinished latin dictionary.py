f7dff93192bbd1e4f65884afb8aefe881a4a3fb2
Leem958 <84033321+Leem958@users.noreply.github.com>
2021-05-12 13:22:56 -0400

    Add files via upload

---
+++ unfinished latin dictionary.py
@@ -0,0 +1,49 @@
+print("LIAM'S LATIN DICTIONARY\n")
+
+Nouns = []
+nouns_Dict = {
+"" : ""
+"" : ""
+"" : ""
+"" : ""
+"" : ""
+"" : ""
+"" : ""
+"" : ""
+}
+Nouns.append(nouns_Dict)
+
+
+Verbs = []
+verbs_Dict = {
+  "love" : "amo, amare, amavi, amatum",
+  "abandon" : "relinquo, reliquere, relinqui, relictum",
+  "able" : "possum, posse, potui",
+  "make" : "facio, facere, feci, factum",
+  "advise" : "moneo, monere, monui, monitum ",
+  "answer" : "respondeo, repondere, respondi, responsum"
+   }
+Verbs.append(verbs_Dict)
+
+
+Adjectives = []
+adjectives_Dict = {
+"" : ""
+"" : ""
+"" : ""
+"" : ""
+"" : ""
+"" : ""
+"" : ""
+"" : ""
+
+}
+Adjectives.append(adjectives_Dict)
+Dictionary = [Nouns, Verbs, Adjectives]
+
+print(verbs_Dict["answer"])
+
+
+
+
+
